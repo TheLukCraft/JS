de91ca7a3d5928fb052de25e7bc0aeb2af6c5221
TheLukCraft <luk1997r@o2.pl>
2024-03-22 17:03:43 +0100

    const and let

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// Comments
+// ! Comments
 
-// console.log and console
+// ! console.log and console
 console.log("test");
+
+// ! variables
+const car = 'Audi';
+let type = 'super car';
+
+console.log(car);
+
+
+// ? let can be updated but not re-declared within the same scope.
+// ? const, on the other hand, is used to declare variables whose values 
+// ? are never intended to change. A variable declared with const must 
+// ? be initialized at the time of declaration. The scope of const is 
+// ? also block-scoped, similar to variables declared with let. However, 
+// ? the variable declared with const is read-only and cannot be updated 
+// ? or re-declared.
+
+
