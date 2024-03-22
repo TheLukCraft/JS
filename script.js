c242fcddc2608dd09c73bd6022a541526c2bbce7
TheLukCraft <luk1997r@o2.pl>
2024-03-22 17:24:12 +0100

    Tample string

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// ! Comments
+// * Comments
 
-// ! console.log and console
+// * console.log and console
 console.log("test");
 
-// ! variables
+// * variables
 const car = 'Audi';
 let type = 'super car';
 
@@ -17,5 +17,13 @@ console.log(car);
 // ? also block-scoped, similar to variables declared with let. However, 
 // ? the variable declared with const is read-only and cannot be updated 
 // ? or re-declared.
+// ! DO NOT USE VAR
 
+// * Tample string
+const name = 'Luk'
+const favMeal = 'Chicken'
 
+// ! It's OLD
+console.log(' HI, Im: ' + name + 'my favourite meal is' + favMeal)
+// ! Use "grawis" this instead
+console.log(`Hi I'm ${name} and my favourite meal is ${favMeal}`)
